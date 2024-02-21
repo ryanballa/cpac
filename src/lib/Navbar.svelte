5305f28f0a55618ece0e96dea19214302d005302
Ryan Balla <ryanballa@Ryans-MacBook-Air.local>
2024-02-21 17:01:49 -0600

    Adding logo

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,12 +1,15 @@
 <script>
 	import { page } from '$app/stores';
+	import logo from '$lib/assets/clarendon-park-advisory-council-logo.svg';
 </script>
 
 <header id="navigation" class="p-navigation is-dark">
 	<div class="p-navigation__row">
 		<div class="p-navigation__banner">
 			<div class="p-navigation__tagged-logo">
-				<a class="p-navigation__link" href="/">Clarendon Park Advisory Council</a>
+				<a class="p-navigation__link" href="/"
+					><img class="logo" src={logo} alt="Clarendon Park Advisory Council" /></a
+				>
 			</div>
 			<a href="#navigation" class="p-navigation__toggle--open" title="menu">Menu</a>
 			<a href="#navigation-closed" class="p-navigation__toggle--close" title="close menu"
@@ -34,3 +37,16 @@
 		</nav>
 	</div>
 </header>
+
+<style lang="scss">
+	@media screen and (min-width: $breakpoint-large) {
+		.p-navigation__nav {
+			align-items: end;
+			justify-content: end;
+		}
+	}
+
+	.logo {
+		width: 170px;
+	}
+</style>
