1e6240906a702e4c108b9efd2e196e863948ab58
Ryan Balla <ryanballa@Ryans-MacBook-Air.local>
2024-03-21 22:48:37 -0500

    Add Tennis Page

---
+++ src/routes/programming/tennis/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import PageComponent from '$lib/PageComponent.svelte';
+
+	export let data;
+	$: ({ TennisPage } = data);
+</script>
+
+{#if $TennisPage.fetching}
+	Loading
+{:else}
+	<PageComponent contentData={$TennisPage.data.page[0]?.content} />
+{/if}
