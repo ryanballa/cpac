ccc1402188d2ab697ee7ea8a0f943c18cfa8835d
Ryan Balla <ryan@ryanballa.com>
2025-03-19 16:00:50 -0500

    Adding static page for minutes

---
+++ src/routes/meeting-minutes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import PageComponent from '$lib/PageComponent.svelte';
+
+	export let data;
+	$: ({ MeetingMinutes } = data);
+</script>
+
+{#if $MeetingMinutes.fetching}
+	Loading
+{:else}
+	<PageComponent contentData={$MeetingMinutes.data.page[0]?.content} />
+{/if}
